943035e5eb5fdf94b0a4c4aa6a1901cbe9da4cd9
Rakusa05 <phenix_ds@hotmail.com>
2024-02-03 18:21:28 -0500

    sort the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        paddingX={10}
+        paddingX="10px"
         spacing={3}
       >
         {isLoading &&
